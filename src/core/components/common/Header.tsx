7a213b64a104909f36d50f0268465ea68ab90dc5
Quoc Bao <quocbaodev1102@gmail.com>
2025-10-24 17:56:55 +0700

    Update Header and HomeTitle components to conditionally render elements based on the current pathname. The Header now hides the support icon when on the home page, and the HomeTitle text has been updated for consistency. Adjust layout styles for better responsiveness.

--- src/core/components/common/Header.tsx
+++ src/core/components/common/Header.tsx
@@ -1,15 +1,22 @@
+"use client";
+
 import { _Image } from "@/core/config";
 import { useSidebar } from "@/store/sidebarStore";
 import { LoginButton } from "../ui";
 import Icon from "./Icon";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
     const { toggle } = useSidebar();
 
+    const pathname = usePathname();
+
+    const isHome = pathname === "/";
+
     return (
         <div className="w-full flex gap-2 justify-between px-4 ">
             {/* icon bar */}
-            <div className="flex-shrink-0">
+            <div className="flex-shrink-0 flex">
                 <button
                     onClick={toggle}
                     className="p-1 rounded-lg hover:bg-gray-100/70 transition-colors duration-200 lg:hidden"
@@ -23,16 +30,18 @@ const Header = () => {
             </div>
 
             {/* title */}
-            <div className="flex items-center gap-2">
-                <Icon
-                    src={_Image.icon.icon_headphone}
-                    size={24}
-                    alt="icon-headphone"
-                />
-                <p className="text font-semibold text-[#5E5E5E]">
-                    Giấy Hoàng Anh hỗ trợ
-                </p>
-            </div>
+            {!isHome && (
+                <div className="flex items-center gap-2">
+                    <Icon
+                        src={_Image.icon.icon_headphone}
+                        size={24}
+                        alt="icon-headphone"
+                    />
+                    <p className="text font-semibold text-[#5E5E5E]">
+                        Giấy Hoàng Anh hỗ trợ
+                    </p>
+                </div>
+            )}
 
             {/* button login */}
             <div className="">
